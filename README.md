04deb9f572387ed675eb5a2d2ae0ae6556b2d3e6
Pau Minoves <pau.minoves@gmail.com>
2015-03-30 16:07:23 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+=============================
+Q2A Sort Answers  
+=============================
+
+Description
+-----------
+This is a plugin for **Question2Answer** that has the capability to make answers from the users in the Expert group sticky so they appear on top of the rest of answers, regardless of votes.
+
+Optionally, a CSS to give a diferent look&feel to those answers can be made. 
+
+Limitation 
+-----------
+Answers cannot be made featured on a per answer basis. Currently only the only criteria is answer-from-an-expert.
+
+Disclaimer
+----------
+It is probably okay for production environments, but may not work exactly as expected.  Refunds will not be given.  If it breaks, you get to keep both parts.
+
+About q2A
+---------
+Question2Answer is a free and open source platform for Q&A sites. For more information, visit:
+
+http://www.question2answer.org/
+
---
+++ qa-plugin.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * Plugin Name: Featured Answers
+ * Plugin URI: https://github.com/pau-minoves/q2a-featured-answers
+ * Plugin Update Check URI: https://raw.github.com/pau-minoves/q2a-featured-answers/master/qa-plugin.php
+ * Plugin Description: Keep answers from experts featured on top of the answer list
+ * Plugin Version: 1
+ * Plugin Date: 2015-3-30
+ * Plugin Author: pau-minoves
+ * Plugin Author URI: https://github.com/pau-minoves
+ * Plugin License: GPLv2
+ * Plugin Minimum Question2Answer Version: 1.4
+ */
+
+if (! defined ( 'QA_VERSION' )) { // don't allow this page to be requested directly from browser
+	header ( 'Location: ../../' );
+	exit ();
+}
+
+qa_register_plugin_module ( 'module', 'qa-featured-admin.php', 'qa_featured_admin', 'Featured Questions' );
+qa_register_plugin_layer ( 'qa-featured-layer.php', 'Featured Layer' );
+
+/*
+ Omit PHP closing tag to help avoid accidental output
+ */
